216cbf77c8fbf036fab378e2a5b484047821baf8
Ben H | Nebula <email@officialharris.co.uk>
2021-07-05 18:53:46 +0100

    Push checks for user login function

--- Login-System/Login-System.cpp
+++ Login-System/Login-System.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+// Check user Logged-In
+
+//Function to return true if user details are correct and matching what is in file
+//Else, return false
+
+bool IsLoggedIn() {
+
+    //Gather username + Password into variables
+    std::string username, password, un, pw;
+    std::cout << "Enter Username: ";
+    std::cin >> username; 
+    std::cout << "Enter Password: ";
+    std::cin >> password;
+
+    //Read from logins.txt
+
+    std::ifstream read("E:\Coding_Projects\C++\LoginSystem\Login-System\loigns.txt");
+    std::getline(read, un);
+    std::getline(read, pw);
+
+    //Check if the read line for username / password matches the provided username and password above
+
+    if (un == username && pw == password) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
